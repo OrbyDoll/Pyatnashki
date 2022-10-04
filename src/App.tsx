e1cbec99c60764b6b11951c77b29e87b8c279ea9
tosha <antonkhazin2006@gmail.com>
2022-10-04 20:46:39 +0300

    nichego

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [fieldSize, setFieldSize] = React.useState(4)
+  let field = <div>1</div>;
+  // for (let i = 0; i < fieldSize*fieldSize; i++) {
+  //   field += <div className="table-inner"></div>
+    
+  // }
   return (
     <div className="table">
-      <div className="d-row">
-        <div className="d-col">
-          <div className="d-col-inner">1</div>
-          <div className="d-col-inner">5</div>
-          <div className="d-col-inner">9</div>
-          <div className="d-col-inner">13</div>
-        </div>
-      </div>
-      <div className="d-row">
-        <div className="d-col">
-          <div className="d-col-inner">2</div>
-          <div className="d-col-inner">6</div>
-          <div className="d-col-inner">10</div>
-          <div className="d-col-inner">14</div>
-        </div>
-      </div>
-      <div className="d-row">
-        <div className="d-col">
-          <div className="d-col-inner">3</div>
-          <div className="d-col-inner">7</div>
-          <div className="d-col-inner">11</div>
-          <div className="d-col-inner">15</div>
-        </div>
-      </div>
-      <div className="d-row">
-        <div className="d-col">
-          <div className="d-col-inner">4</div>
-          <div className="d-col-inner">8</div>
-          <div className="d-col-inner">16</div>
-        </div>
-      </div>
+      {field}
     </div>
   );
 }
