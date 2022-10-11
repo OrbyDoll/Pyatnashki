d3e0e4262355eadce24d719436bb2660b5abc42d
tosha <antonkhazin2006@gmail.com>
2022-10-11 20:50:13 +0300

    started

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
-  const [fieldSize, setFieldSize] = React.useState(4)
-  let field = <div>1</div>;
-  // for (let i = 0; i < fieldSize*fieldSize; i++) {
-  //   field += <div className="table-inner"></div>
-    
-  // }
+  const [fieldSize, setFieldSize] = useState([{
+    value:1,
+    position: "0px",
+    moveable: true
+  }])
+  let field = fieldSize.map(square=>{
+    if(typeof square.value == "number"){
+      return <div key = {square.value} className="table-inner" onClick={(e)=>console.log(e)}>{square.value}</div>
+    }else{
+      return <div key = {square.value} className="empty-square"></div>
+    }
+  })
+
   return (
     <div className="table">
       {field}
